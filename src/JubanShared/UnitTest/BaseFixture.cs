421cb8084278616e5bc72c31699fb06bc99b05e9
Zefei Ji <3108252+zefeiji@users.noreply.github.com>
2021-04-21 04:23:38 +0100

    x

--- src/JubanShared/UnitTest/BaseFixture.cs
+++ src/JubanShared/UnitTest/BaseFixture.cs
@@ -6,6 +6,7 @@ namespace Jubanlabs.JubanShared.UnitTest
     {
         public BaseFixture()
         {
+            AppSession.Instance.SetEnvironmentName( "testing");
         }
 
         public void Dispose()
